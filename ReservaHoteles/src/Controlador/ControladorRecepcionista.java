26d3f81aecccdc918b6210631d8a97ba15a02c9f
Diego <diego.mcastro4@gmail.com>
2020-06-19 01:35:24 -0500

    controladorRecepcionista

--- ReservaHoteles/src/Controlador/ControladorRecepcionista.java
+++ ReservaHoteles/src/Controlador/ControladorRecepcionista.java
@@ -1,30 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controlador;
+
+import Modelo.Recepcionista;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  *
  * @author jhonn
  */
 public class ControladorRecepcionista {
-    
-    public void registrarRecepcionista() {
 
+    private int id;
+
+    private SortedMap listaRecepcionista;
+    int cont;
+
+    public ControladorRecepcionista() {
+        listaRecepcionista = new TreeMap();
+        cont = 1;
     }
 
-    public void eliministrarRecepcionista() {
-
+    public SortedMap getListaPersona() {
+        return listaRecepcionista;
     }
 
-    public void buscarRecepcionista() {
-
+    public void setListaRecepcionista(SortedMap listaRecepcionista) {
+        this.listaRecepcionista = listaRecepcionista;
     }
 
-    public void actualizarRecepcionista() {
-
+    public void crear(int codigo, Recepcionista l) {
+        listaRecepcionista.put(codigo, l);
+        cont++;
     }
-    
+
+    public void actualizar(int codigo, Recepcionista nuevo) {
+        listaRecepcionista.replace(codigo, nuevo);
+    }
+
+    public Recepcionista leer(int codigo) {
+        Recepcionista l = (Recepcionista) listaRecepcionista.get(codigo);
+        return l;
+    }
+
+    public void eliminar(int codigo) {
+        listaRecepcionista.remove(codigo);
+    }
+
+    public int getCont() {
+        return cont;
+    }
+
+    public void setCont(int cont) {
+        this.cont = cont;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
